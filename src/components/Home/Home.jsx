2e379fe5e2f974f48465fb231cea393ca43e628e
Ziad <ZiadElsayedAsr@gmail.com>
2024-01-25 09:06:27 +0200

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,60 +1,78 @@
-import {TbArrowBigRightLines} from 'react-icons/tb'
-import {TbChevronsDown} from 'react-icons/tb'
-import {AiFillGithub} from 'react-icons/ai'
-import {AiFillLinkedin} from 'react-icons/ai'
+import { TbArrowBigRightLines } from "react-icons/tb";
+import { TbChevronsDown } from "react-icons/tb";
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 
-import './Home.scss'
+import "./Home.scss";
 
 const Home = () => {
   return (
-    <section id='home' className='home section'>
-      
+    <section id="home" className="home section">
       <div className="leftIcons">
         <div className="socials grid">
-            <a href="https://www.linkedin.com/in/ziad-elsayed-0ziad0" rel="noreferrer noopener"  target="_blank"><AiFillLinkedin className="icon" /></a>
-            <a href="https://github.com/Ziad-Asr" rel="noreferrer noopener" target="_blank"><AiFillGithub className="icon" /></a>
-           <div className="line"></div>
+          <a
+            href="https://www.linkedin.com/in/ziad-elsayed-asr"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            <AiFillLinkedin className="icon" />
+          </a>
+          <a
+            href="https://github.com/Ziad-Asr"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            <AiFillGithub className="icon" />
+          </a>
+          <div className="line"></div>
         </div>
       </div>
 
       <div className="container homeContent">
-        <span className="introText">
-          Hi my name is,
-        </span>
-        <h1 className="title">
-          Ziad Elsayed
-        </h1>
+        <span className="introText">Hi my name is,</span>
+        <h1 className="title">Ziad Elsayed</h1>
         <span className="subTitle">
           I develope scalable web based applications and software.
         </span>
         <p className="homeParagraph">
-        I code within required coding standards and styles to serve the exact need.
+          I code within required coding standards and styles to serve the exact
+          need.
         </p>
 
         <div className="lowerHomeSection">
-          <button className='contactBtn '>
-            <a className='flex'  href="#contact">
-            Contact Me <TbArrowBigRightLines className='icon'/>
+          <button className="contactBtn ">
+            <a className="flex" href="#contact">
+              Contact Me <TbArrowBigRightLines className="icon" />
             </a>
           </button>
 
           <div class="scrollDiv">
-              <a href="#about" className='flex'> <h6 class="scroll">Scroll Down <TbChevronsDown className='icon'/></h6></a>   
-           </div>
+            <a href="#about" className="flex">
+              {" "}
+              <h6 class="scroll">
+                Scroll Down <TbChevronsDown className="icon" />
+              </h6>
+            </a>
+          </div>
         </div>
       </div>
 
       <div className="rightEmail">
         <div>
-          <div className='emailAddress'>
-            <a href="https://mailto:ZiadElsayedAsr@gmail.com" rel="noreferrer noopener" target="_blank">ZiadElsayedAsr@gmail.com</a>
+          <div className="emailAddress">
+            <a
+              href="https://mailto:ZiadElsayedAsr@gmail.com"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              ZiadElsayedAsr@gmail.com
+            </a>
           </div>
-           <div className="line"></div>
+          <div className="line"></div>
         </div>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,132 +1,108 @@
-import {FiGithub} from 'react-icons/fi'
-import FoodApp from '../../assests/FoodApp-image.jpg'
-import car from '../../assests/Car-Rental.jfif'
-import NFT from '../../assests/NFT-Image.jpg'
-import weather from '../../assests/weather.jpg'
-import Todo from '../../assests/Todo-image.jpg'
+import { FiGithub } from "react-icons/fi";
+import FoodApp from "../../assests/FoodApp-image.jpg";
+import car from "../../assests/Car-Rental.jfif";
+import NFT from "../../assests/NFT-Image.jpg";
+import weather from "../../assests/weather.jpg";
+import Todo from "../../assests/Todo-image.jpg";
 
-import './Projects.scss'
+import "./Projects.scss";
 
 const data = [
   {
     id: 1,
     image: FoodApp,
-    liveLink: 'https://ziad-food-app.netlify.app',
-    github: 'https://github.com/Ziad-Asr/Food-App',
-    title: 'Food App',
-    desc: 'This is a food delivary app for fast food.',
-    tech1: 'React',
-    tech2: 'Javascript',
-    tech3: 'scss',
-  },
-  {
-    id: 5,
-    image: car,
-    liveLink: 'https://ziad-car-rental.netlify.app',
-    github: 'https://github.com/Ziad-Asr/Car-rental-App',
-    title: 'Car-rental App',
-    desc: 'An app for renting cars',
-    tech1: 'React',
-    tech2: 'Javascript',
-    tech3: 'css',
+    liveLink: "https://ziad-food-app.netlify.app",
+    github: "https://github.com/Ziad-Asr/Food-App",
+    title: "Food App",
+    desc: "This is a food delivary app for fast food.",
+    tech1: "React",
+    tech2: "Javascript",
+    tech3: "scss",
   },
   {
     id: 2,
-    image: Todo,
-    liveLink: 'https://ziad-todos.netlify.app/',
-    github: 'https://github.com/Ziad-Asr/Todo-list',
-    title: 'Todo list',
-    desc: 'Todo list for daily tasks.',
-    tech1: 'React',
-    tech2: 'Javascript',
-    tech3: 'scss',
+    image: car,
+    liveLink: "https://ziad-car-rental.netlify.app",
+    github: "https://github.com/Ziad-Asr/Car-rental-App",
+    title: "Car-rental App",
+    desc: "An app for renting cars",
+    tech1: "React",
+    tech2: "Javascript",
+    tech3: "css",
   },
-  {
-    id: 4,
-    image: NFT,
-    liveLink: 'https://ziad-nft-store.netlify.app',
-    github: 'https://github.com/Ziad-Asr/NFT-Store',
-    title: 'NFT Store',
-    desc: 'NFT  Store for unique images',
-    tech1: 'React',
-    tech2: 'Javascript',
-    tech3: 'scss',
-  },
-<<<<<<< HEAD
   {
     id: 3,
-    image: weather,
-    liveLink: 'https://ziad-weather-app.netlify.app/',
-    github: 'https://github.com/Ziad-Asr/Weather-App',
-    title: 'Weather App',
-    desc: 'Weather app for all countries.',
-    tech1: 'React',
-    tech2: 'Javascript',
-    tech3: 'scss',
+    image: NFT,
+    liveLink: "https://ziad-nft-store.netlify.app",
+    github: "https://github.com/Ziad-Asr/NFT-Store",
+    title: "NFT Store",
+    desc: "NFT  Store for unique images",
+    tech1: "React",
+    tech2: "Javascript",
+    tech3: "scss",
   },
-=======
-  // {
-  //   id: 3,
-  //   image: weather,
-  //   liveLink: 'https://ziad-weather-app.netlify.app/',
-  //   github: 'https://github.com/Ziad-Asr/Weather-App',
-  //   title: 'Weather App',
-  //   desc: 'Weather app for all countries.',
-  //   tech1: 'React',
-  //   tech2: 'Javascript',
-  //   tech3: 'scss',
-  // },
->>>>>>> 4dea1e90639924999c3b830c59b2133a52b89618
-]
+];
 
 const Project = () => {
   return (
-    <section id='projects' className='projects container section'>
+    <section id="projects" className="projects container section">
       <div class="sectionTitle">
-        <span class="titleNumber">03 . </span>  
-        <h5 class="titleText">Projects <div class="underline"><span></span></div></h5>
+        <span class="titleNumber">03 . </span>
+        <h5 class="titleText">
+          Personal projects
+          <div class="underline">
+            <span></span>
+          </div>
+        </h5>
       </div>
 
-      <div className="projectContainer grid"> 
-          { 
-          data.map(({id, github, image, liveLink, desc, title, tech1, tech2, tech3}) =>{
+      <div className="projectContainer grid">
+        {data.map(
+          ({
+            id,
+            github,
+            image,
+            liveLink,
+            desc,
+            title,
+            tech1,
+            tech2,
+            tech3,
+          }) => {
             return (
               <div key={id} className="singleProject">
                 <div className="externalLinks flex">
                   <div className="githubIcon">
-                  <a href={github} rel="noreferrer noopener" target="_blank"><FiGithub className="icon"/></a>
+                    <a href={github} rel="noreferrer noopener" target="_blank">
+                      <FiGithub className="icon" />
+                    </a>
+                  </div>
+                </div>
+
+                <div className="imgDiv">
+                  <a href={liveLink} rel="noreferrer noopener" target="_blank">
+                    <img src={image} alt={title} />
+                  </a>
+                </div>
+
+                <div className="projectTitle">
+                  <h3>{title}</h3>
+                </div>
+
+                <div className="desc">{desc}</div>
+
+                <div className="technologies flex">
+                  <small> {tech1} </small>
+                  <small> {tech2} </small>
+                  <small> {tech3} </small>
                 </div>
               </div>
-              
-             <div className="imgDiv">
-              <a href={liveLink} rel="noreferrer noopener" target="_blank">
-              <img src={image} alt={title} /> 
-              </a>
-             </div>
-
-             <div className="projectTitle">
-              <h3>{title}</h3>
-             </div>
-
-             <div className="desc">
-              {desc}
-             </div>
-
-             <div className="technologies flex">
-                <small> {tech1} </small>
-                <small> {tech2} </small>
-                <small> {tech3} </small>                
-             </div>
-             
-             </div>
-            )
-
-           })
-           }
+            );
+          }
+        )}
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
